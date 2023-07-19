24a87421182df77b8e69813c7ed72b7d24d0c5de
altarzzzz <139721884+altarzzzz@users.noreply.github.com>
2023-07-19 02:46:49 -0400

    Update Grabber.py

--- Grabber.py
+++ Grabber.py
@@ -15,7 +15,7 @@ print("Coded by dev github/dev6920")
 
 while True:
   
-  username = input("Enter username (type 'exit' to quit): ")
+  username = input(boxanu)
 
   if username == "exit":
     break
